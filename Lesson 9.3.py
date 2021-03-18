4d9f0fc443e67f422da1e309911e918aa93a3379
Pavel Shak <Pshak94@yandex.ru>
2021-03-18 20:07:41 +0300

    9.3 fix

--- Lesson 9.3.py
+++ Lesson 9.3.py
@@ -10,67 +10,25 @@ def get_data(file_name):
     return data
 
 
-def files_merge(file_name1, file_name2, file_name3,  merge_file):
-    str_count1 = lines_count(file_name1)
-    str_count2 = lines_count(file_name2)
-    str_count3 = lines_count(file_name3)
-    if str_count1 == min(str_count1, str_count2, str_count3):
-        min_file = file_name1
-        min_str = str_count1
-        if str_count2 == max(str_count1, str_count2, str_count3):
-            max_file = file_name2
-            max_str = str_count2
-            mid_file = file_name3
-            mid_str = str_count3
-        else:
-            max_file = file_name3
-            max_str = str_count3
-            mid_file = file_name2
-            mid_str = str_count2
-    elif str_count1 == max(str_count1, str_count2, str_count3):
-        max_file = file_name1
-        max_str = str_count1
-        if str_count2 == min(str_count1, str_count2, str_count3):
-            min_file = file_name2
-            min_str = str_count2
-            mid_file = file_name3
-            mid_str = str_count3
-        else:
-            min_file = file_name3
-            min_str = str_count3
-            mid_file = file_name2
-            mid_str = str_count2
-    elif str_count2 == min(str_count1, str_count2, str_count3):
-        min_file = file_name2
-        min_str = str_count2
-        max_file = file_name3
-        max_str = str_count3
-        mid_file = file_name1
-        mid_str = str_count1
-    else:
-        min_file = file_name3
-        min_str = str_count3
-        max_file = file_name2
-        max_str = str_count2
-        mid_file = file_name1
-        mid_str = str_count1
-    data_min = get_data(min_file)
-    data_mid = get_data(mid_file)
-    data_max = get_data(max_file)
-
-    with open(merge_file, "w", encoding='utf-8') as f:
-        f.write(min_file + '\n')
-        f.write(str(min_str) + '\n')
-        f.write(data_min + '\n')
-        f.write(mid_file + '\n')
-        f.write(str(mid_str) + '\n')
-        f.write(data_mid + '\n')
-        f.write(max_file + '\n')
-        f.write(str(max_str) + '\n')
-        f.write(data_max + '\n')
-
+def files_merge(files_list, merge_file):
+    files_dict = {}
+    files_len_list = []
+    for file in files_list:
+        file_len = lines_count(file)
+        files_dict[file] = file_len
+        files_len_list.append(file_len)
+    while len(files_dict) != 0:
+        for file, str_count in files_dict.copy().items():
+            if str_count == min(files_len_list):
+                data = get_data(file)
+                with open(merge_file, "a", encoding='utf-8') as f:
+                    f.write(file + '\n')
+                    f.write(str(str_count) + '\n')
+                    f.write(data + '\n')
+                files_dict.pop(file)
+                files_len_list.remove(str_count)
     return get_data(merge_file)
 
 
-# print(files_merge("1.txt", "2.txt", "3.txt", "Merge.txt"))
+print(files_merge(["1.txt", "2.txt", "3.txt"], "Merge.txt"))
 
